f02518bc74395a3f7cc69ed2e2eca9010f69b7f0
IreneKhymych <himicirina1@gmail.com>
2024-02-22 01:16:09 +0200

    funcion

---
+++ utils.py
@@ -0,0 +1,17 @@
+def factorial(n): 
+	for i in range(1, n + 1):
+		fact *= i
+	return fact
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number == 2:
+        return True
+    elif number % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(number**0.5) + 1, 2):
+            if number % i == 0:
+                return False
+        return True
